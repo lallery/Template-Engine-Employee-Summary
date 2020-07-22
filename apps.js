2193b958075881d3657b77f6f268e3f5b7fa30f9
lallery <64818633+lallery@users.noreply.github.com>
2020-07-21 20:20:22 -0400

    Create apps.js

---
+++ apps.js
@@ -0,0 +1,38 @@
+const inquirer = require("inquirer");
+const fs = require("fs");const Employee = require("./lib/Employee");
+
+const Employee = require("./lib/Employee")
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+
+
+
+
+function promptUser() {
+    return inquirer.prompt([
+        {
+            type: "input",
+            message: "Enter your first name:",
+            name: "name"
+        }, {
+            type: "input",
+            message: "Enter your email address:",
+            name: "email"
+        }, {
+            type: "input",
+            message: "What's your role within the company?",
+            name: "role",
+            choices: ['engineer', 'intern', 'manager']
+            //conditional statement about employment: intern ask for school, if manager ask for office number, if engineer ask for githubname
+        },
+        {
+            type: "input",
+            message: "Enter your github username:",
+            name: "username"
+        }, ]);
+}
+
+// let readyToConvert = false;
+// promptUser()
+//     .then(function ({ name, email, role, username }) {
